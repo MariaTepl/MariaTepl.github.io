c3ac0ddcb64cd36597fe8fb080402af37b8b1cd4
MidooLya <kphd984@gmail.com>
2025-02-03 20:52:15 +0700

    Update component5.js

--- component5.js
+++ component5.js
@@ -1,34 +1,34 @@
 AFRAME.registerComponent('collectible', {
-        init: function () {
-            this.el.addEventListener('click', () => {
-                this.el.parentNode.removeChild(this.el);
-                let scoreElement = document.querySelector('[text][value^="Score:"]');
-                if (scoreElement) {
-                    let score = parseInt(scoreElement.getAttribute('text').value.split(': ')[1]) || 0;
-                    scoreElement.setAttribute('text', { value: 'Score: ' + (score + 1) });
-                }
-            });
-    
-            let timeLeft = 60;
-            let timerElement = document.querySelector('#timer');
-    
-            function updateTimer() {
-                if (timeLeft > 0) {
-                    timeLeft--;
-                    if (timerElement) {
-                        timerElement.setAttribute('text', { value: 'Time: ' + timeLeft });
+            init: function () {
+                this.el.addEventListener('click', () => {
+                    this.el.parentNode.removeChild(this.el);
+
+                    // Обновление счета
+                    let scoreElement = document.querySelector('#score');
+                    let score = parseInt(scoreElement.getAttribute('value').split(': ')[1]) + 1;
+                    scoreElement.setAttribute('value', 'Score: ' + score);
+                });
+                let timeLeft = 60; //
+                let timerElement = document.querySelector('#timer');
+        
+                function updateTimer() {
+                    if (timeLeft > 0) {
+                        timeLeft--;
+                        timerElement.setAttribute('value', 'Time: ' + timeLeft);
+                    } else {
+                        endGame();
                     }
-                } else {
-                    endGame();
+                }
+        
+                setInterval(updateTimer, 1000);
+        
+                function endGame() {
+                    let finalScore = parseInt(document.querySelector('#score').getAttribute('value').split(': ')[1]);
+                    let gameOverText = document.querySelector('#gameOverText');
+                    gameOverText.setAttribute('value', 'Game Over! Your final score is: ' + finalScore);
+        
+                    // Перезапуск игры (можно убрать, если не нужно)
+                    // location.reload();
                 }
             }
-    
-            setInterval(updateTimer, 1000);
-    
-            function endGame() { 
-            let finalScore = parseInt(document.querySelector('[text_value="Score: "]').getAttribute('text').value.split(': ')[1]); 
-            alert('Game Over! Your final score is: ' + finalScore); 
-            location.reload();
-        }
-        }
-    });
+        });
