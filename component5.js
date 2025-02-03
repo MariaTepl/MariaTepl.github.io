2b769a3ae568fe9a8f5a3ce670c4be52ad061b10
MidooLya <kphd984@gmail.com>
2025-02-03 21:17:51 +0700

    Update component5.js

--- component5.js
+++ component5.js
@@ -8,25 +8,26 @@ AFRAME.registerComponent('collectible', {
                     let score = parseInt(scoreElement.getAttribute('value').split(': ')[1]) + 1;
                     scoreElement.setAttribute('value', 'Score: ' + score);
                 });
-                let timeLeft = 60; //
-                let timerElement = document.querySelector('#timer');
-        
-                function updateTimer() {
-                    if (timeLeft > 0) {
-                        timeLeft--;
-                        timerElement.setAttribute('value', 'Time: ' + timeLeft);
-                    } else {
-                        endGame();
-                    }
-                }
-        
-                setInterval(updateTimer, 1000);
-        
-                function endGame() {
-                    let finalScore = parseInt(document.querySelector('#score').getAttribute('value').split(': ')[1]);
-                    let gameOverText = document.querySelector('#gameOverText');
-                    gameOverText.setAttribute('value', 'Game Over! Your final score is: ' + finalScore);
-                    location.reload();
-                }
             }
         });
+
+        let timeLeft = 60;
+        let timerElement = document.querySelector('#timer');
+
+        function updateTimer() {
+            if (timeLeft > 0) {
+                timeLeft--;
+                timerElement.setAttribute('value', 'Time: ' + timeLeft);
+            } else {
+                endGame();
+            }
+        }
+
+        setInterval(updateTimer, 1000);
+
+        function endGame() {
+            let finalScore = parseInt(document.querySelector('#score').getAttribute('value').split(': ')[1]);
+            let gameOverText = document.querySelector('#gameOverText');
+            gameOverText.setAttribute('value', 'Game Over! Your final score is: ' + finalScore);
+            // location.reload();
+        }
